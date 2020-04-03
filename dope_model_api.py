2abd427e39fe26db7c8d9bec3ef2e1eebd2d5be4
Ian Myjer <imyjer@gmail.com>
2020-04-03 09:24:52 -0400

    fixup

--- dope_model_api.py
+++ dope_model_api.py
@@ -1,8 +1,8 @@
 # %%
-from fastapi import FastAPI, HTTPException
+from fastapi import FastAPI
 from pydantic import BaseModel
 import pandas as pd
-import pickle
+import joblib
 
 app = FastAPI()
 
@@ -10,7 +10,8 @@ app = FastAPI()
 # %%
 @app.post('/predict/', status_code=200)
 def model_predict():
-    # TODO: load pickled model
-    # TODO: parse data passed by user
+    # TODO: load pickled model (and any transformers)
+    # TODO: intake and process data passed by user
     # TODO: make a model prediction and return results!
+    # TODO: Extra Credit: How would you write this to intake mulitiple rows of data and return multiple predictions?
     pass
