8686732664c003fa0f2465253b2fbe5bb195869d
Ian Myjer <imyjer@gmail.com>
2020-04-03 10:20:26 -0400

    update to handle multiple rows of data

--- dope_model_api.py
+++ dope_model_api.py
@@ -1,11 +1,14 @@
 from fastapi import FastAPI
 from pydantic import BaseModel
+from typing import List
 from joblib import load
 import pandas as pd
+import json
 
 app = FastAPI()
 
 
+# Define individual row classes
 class Iris(BaseModel):
     sepal_length: float
     sepal_width: float
@@ -13,17 +16,22 @@ class Iris(BaseModel):
     petal_width: float
 
 
+# define data class
+class Irises(BaseModel):
+    data: List[Iris]
+
+
 @app.post('/predict/', status_code=200)
-async def model_predict(iris: Iris):
+async def model_predict(irises: Irises):
     # le = load('iris_le.joblib')
     # pred = rfc.predict(row)
     # return le.inverse_transform(pred)[0]
 
-    # load row passed into df
-    row = pd.DataFrame([iris.dict()])
+    # load row(s) passed into df
+    row = pd.DataFrame(irises.dict()['data'])
 
     # load pickled files
     rfc = load('iris_rfc.joblib')
 
-    # make prediction
-    return rfc.predict(row)[0]
+    # make prediction(s)
+    return json.dumps(list(rfc.predict(row)))
