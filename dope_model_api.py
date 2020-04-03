2f87815e52cde3279d8d405de3430db30a7d0394
Ian Myjer <imyjer@gmail.com>
2020-04-02 23:36:29 -0400

    ian's dope model

--- dope_model_api.py
+++ dope_model_api.py
@@ -1,16 +1,30 @@
-# %%
-from fastapi import FastAPI, HTTPException
+from fastapi import FastAPI
 from pydantic import BaseModel
+from joblib import load
 import pandas as pd
-import pickle
 
 app = FastAPI()
 
 
-# %%
+class Iris(BaseModel):
+    sepal_length: float
+    sepal_width: float
+    petal_length: float
+    petal_width: float
+
+
 @app.post('/predict/', status_code=200)
-def model_predict():
-    # TODO: load pickled model
-    # TODO: parse data passed by user
-    # TODO: make a model prediction and return results!
-    pass
+async def model_predict(iris: Iris):
+
+    # load row passed into df
+    row = pd.DataFrame([iris.dict()])
+
+    # load pickled files
+    rfc = load('iris_rfc.joblib')
+    le = load('iris_le.joblib')
+
+    # make prediction
+    pred = rfc.predict(row)
+
+    # return string prediction
+    return le.inverse_transform(pred)[0]
